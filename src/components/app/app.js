166adab274f7bef6746aac54e413c8ed07823ced
Your Name <you@example.com>
2020-10-21 22:45:56 +0600

    Caroussel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Header from '../header';
+import Carousel from '../carousel';
 
 class App extends Component {
     render() {
         return (
             <div>
                <Header/>,
+                <Carousel/>,
             </div>
         );
     }
